f7ace4bea67fac95800dd506495f05753ab04f7c
funchcode <funchcode@gmail.com>
2023-11-21 17:51:11 +0900

    chore: user - 도메인 타입 추가
    추후에 user 인증 쪽은 소스 비공개로 전환 필요. user 빌드 패턴 적용 가능한지 검토 필요

--- internal/user/service.go
+++ internal/user/service.go
@@ -2,6 +2,17 @@ package user
 
 // 회원가입
 // 로그인
+type service struct {
+}
 
-type Service struct {
+func NewService() *service {
+	return &service{}
+}
+
+func (*service) SignUp() {
+	println("회원가입 진행")
+}
+
+func (*service) SignIn() {
+	println("로그인 진행")
 }
---
+++ internal/user/user.go
@@ -0,0 +1,11 @@
+package user
+
+type user struct {
+	Id      string
+	Email   string
+	Name    string
+	Phone   string
+	Age     uint8
+	Gender  uint8
+	Deleted bool
+}
